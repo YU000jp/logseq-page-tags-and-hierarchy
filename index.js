da8f8f4a3f9a45d94b7fbdbdde481b8e1f07a2a3
YU <111847207+YU000jp@users.noreply.github.com>
2022-10-15 13:16:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function main(){logseq.provideStyle(String.raw`
 /* Screen size */
 @media screen and (min-width: 1580px) {
-        main:not(.ls-right-sidebar-open) div#main-content-container div.flex-1.page.relative {
+        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.flex-1.page.relative {
             margin-right: 390px;
             margin-bottom: 2em;
             margin-top: 2em;
             margin-left: 1.5em;
         }
         /* right-space */
-        main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
-        main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
+        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
+        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
             position: fixed;
             width: 390px;
             max-height: 40vh;
@@ -20,21 +20,24 @@ function main(){logseq.provideStyle(String.raw`
             border-radius: 10px;
         }
         /* Pages-tagged-with */
-        main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
+        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
             top: 6.5em;
             right: 1em;
         }
+        body.is-pdf-active div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
         main.ls-right-sidebar-open div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
             display: none;
         }
-        body:not(.is-tabs-loaded) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
+        body:not(.is-tabs-loaded.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
             top: 4.5em;
         }
         /* page-hierarchy */
+        body:not(.is-pdf-active) div#main-content-container div.page-hierarchy,
         main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
             right: 1em;
             bottom: 3em;
         }
+        body.is-pdf-active div#main-content-container div.page-hierarchy,
         main.ls-right-sidebar-open div#main-content-container div.page-hierarchy {
             display: none;
         }
