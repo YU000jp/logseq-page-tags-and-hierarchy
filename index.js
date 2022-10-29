100c41841481d9a318593f81a00bca7f03de6087
YU <111847207+YU000jp@users.noreply.github.com>
2022-10-29 16:32:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 function main(){logseq.provideStyle(String.raw`
 /* Screen size */
-@media screen and (min-width: 1580px) {
+@media screen and (max-width: 1624px) {
         body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.flex-1.page.relative {
-            margin-right: 390px;
             margin-bottom: 2em;
             margin-top: 2em;
             margin-left: 1.5em;
+        }
+        /* right-space */
+        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
+        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
+            position: fixed;
+            height: 210px;
+            width: 42vw;
+            overflow-y: auto;
+            padding: 1.5em;
+            font-size: 0.95em;
+            background-color: var(--ls-primary-background-color);
+            border-radius: 10px;
+            z-index: +1;
+        }
+        /* Pages-tagged-with */
+        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
+            bottom: 0;
+            right: 0.3em;
+        }
+        /* page-hierarchy */
+        body:not(.is-pdf-active) div#main-content-container div.page-hierarchy,
+        main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
+            right: 42vw;
+            bottom: 0;
+        }
+}
+@media screen and (min-width: 1625px) {
+        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.flex-1.page.relative {
+            margin-bottom: 2em;
+            margin-top: 2em;
+            margin-left: 1.5em;
+        }
+        body:not(.is-pdf-active) main.ls-wide-mode div#main-content-container div.flex-1.page.relative {
+            margin-right: 390px;
         }
         /* right-space */
         body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
@@ -24,10 +57,6 @@ function main(){logseq.provideStyle(String.raw`
             top: 6.5em;
             right: 1em;
         }
-        body.is-pdf-active div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
-        main.ls-right-sidebar-open div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
-            display: none;
-        }
         body:not(.is-tabs-loaded.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
             top: 4.5em;
         }
@@ -37,14 +66,14 @@ function main(){logseq.provideStyle(String.raw`
             right: 1em;
             bottom: 3em;
         }
+}
+/* Screen size Finish */
+        body.is-pdf-active div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
+        main.ls-right-sidebar-open div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
         body.is-pdf-active div#main-content-container div.page-hierarchy,
-        main.ls-right-sidebar-open div#main-content-container div.page-hierarchy {
-            display: none;
-        }
+        main.ls-right-sidebar-open div#main-content-container div.page-hierarchy,
         div#right-sidebar div.relative+div.references.mt-6.flex-1.flex-row,
         div#right-sidebar div.page-hierarchy {
             display: none;
         }
-}
-/* Screen size Finish */
 `);}logseq.ready(main).catch(console.error)
