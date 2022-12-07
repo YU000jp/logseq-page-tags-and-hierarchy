a943ce462c7d4e3bca79781c6a4eca3d780ef1fe
YU000jp <111847207+YU000jp@users.noreply.github.com>
2022-12-07 16:50:39 +0900

    設定画面を追加
    設定画面から、横に配置するか、下側にするか選択できるようになった

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "@logseq/libs";
+import { settingUI } from "./setting";
 
 const main = () => {
+    settingUI(); /* -setting */
     /* CSS */
     logseq.provideStyle(String.raw`
     /* Logseq bugs fix */
@@ -20,6 +22,54 @@ const main = () => {
         background-color: unset;
     }
     
+    /* CANCEL PDF view & right sidebar */
+    body.is-pdf-active div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
+    main.ls-right-sidebar-open div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
+    body.is-pdf-active div#main-content-container div.page-hierarchy,
+    main.ls-right-sidebar-open div#main-content-container div.page-hierarchy,
+    div#right-sidebar div.relative+div.references.mt-6.flex-1.flex-row,
+    div#right-sidebar div.page-hierarchy {
+        display: none;
+    }
+    `);
+    const Setting = logseq.settings.switch01;
+    if (Setting !== undefined && Setting === "disable") {
+    logseq.provideStyle(String.raw`
+    body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.flex-1.page.relative {
+        margin-bottom: 2em;
+        margin-top: 2em;
+        margin-left: 1.5em;
+    }
+
+    /* right-space */
+    body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
+    body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
+        position: fixed;
+        max-height: 210px;
+        width: 42vw;
+        overflow-y: auto;
+        padding: 1.5em;
+        font-size: 0.95em;
+        background-color: var(--ls-primary-background-color);
+        border-radius: 10px;
+        z-index: var(--ls-z-index-level-3);
+    }
+
+    /* Pages-tagged-with */
+    body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
+        bottom: 0;
+        right: 0.3em;
+    }
+
+    /* page-hierarchy */
+    body:not(.is-pdf-active) div#main-content-container div.page-hierarchy,
+    main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
+        right: 42vw;
+        bottom: 0;
+    }
+    `);
+    }else{
+    logseq.provideStyle(String.raw`
     /* Screen size */
     @media screen and (max-width: 1624px) {
         body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.flex-1.page.relative {
@@ -102,17 +152,9 @@ const main = () => {
             right: 1em;
             bottom: 3em;
         }
+    }    
+    `);
     }
-    
-    /* Screen size Finish */
-    body.is-pdf-active div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
-    main.ls-right-sidebar-open div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
-    body.is-pdf-active div#main-content-container div.page-hierarchy,
-    main.ls-right-sidebar-open div#main-content-container div.page-hierarchy,
-    div#right-sidebar div.relative+div.references.mt-6.flex-1.flex-row,
-    div#right-sidebar div.page-hierarchy {
-        display: none;
-    }
-  `);
+
 };
 logseq.ready(main).catch(console.error);
---
+++ src/setting.js
@@ -0,0 +1,22 @@
+export const settingUI = () => {
+    /* https://logseq.github.io/plugins/types/SettingSchemaDesc.html */
+    const settingsTemplate = [
+        {
+            key: "heading00",
+            title: "*Please reboot Logseq to reflect styles.",
+            type: "heading",
+            default: "",
+            description: "",
+        },
+        {
+            key: "switch01",
+            title: "side by side *",
+            type: "enum",
+            enumChoices: ["enable", "disable"],
+            enumPicker: "radio",
+            default: "enable",
+            description: "enable: Window size limit [min-width 1625px]  | disable: Place it on the bottom side.",
+        }
+    ];
+    logseq.useSettingsSchema(settingsTemplate);
+};
