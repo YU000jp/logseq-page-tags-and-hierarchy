8500ad729cdaa6221e7e630daa16de0609de4202
YU000jp <111847207+YU000jp@users.noreply.github.com>
2022-12-08 11:32:04 +0900

    Update CSS
    大幅なデザインの変更

--- src/index.js
+++ src/index.js
@@ -6,29 +6,30 @@ const main = () => {
     /* CSS */
     logseq.provideStyle(String.raw`
     /* Logseq bugs fix */
+    
     /* Fix "Extra space when journal queries are not active #6773" */
-    div#journals div#today-queries>div.lazy-visibility {
+    body[data-page="home"] div#today-queries>div.lazy-visibility {
         min-height: unset !important;
     }
     
-    /* journal queries */
-    div#journals div#today-queries>div.lazy-visibility>div.shadow {
+    /* journal queries No shadow */
+    body[data-page="home"] div#today-queries>div.lazy-visibility>div.shadow {
         display: none;
     }
     
     /* background conflict journal queries */
-    div#journals div#today-queries div.color-level div.blocks-container,
-    div#journals div#today-queries div.color-level {
+    body[data-page="home"] div#today-queries div.color-level div.blocks-container,
+    body[data-page="home"] div#today-queries div.color-level {
         background-color: unset;
     }
     
     /* CANCEL PDF view & right sidebar */
-    body.is-pdf-active div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
-    main.ls-right-sidebar-open div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
-    body.is-pdf-active div#main-content-container div.page-hierarchy,
-    main.ls-right-sidebar-open div#main-content-container div.page-hierarchy,
-    div#right-sidebar div.relative+div.references.mt-6.flex-1.flex-row,
-    div#right-sidebar div.page-hierarchy {
+    body[data-page="page"].is-pdf-active div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
+    body[data-page="page"] main.ls-right-sidebar-open div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
+    body[data-page="page"].is-pdf-active div#main-content-container div.page-hierarchy,
+    body[data-page="page"] main.ls-right-sidebar-open div#main-content-container div.page-hierarchy,
+    body[data-page="page"] div#right-sidebar div.relative+div.references.mt-6.flex-1.flex-row,
+    body[data-page="page"] div#right-sidebar div.page-hierarchy {
         display: none;
     }
     `);
@@ -39,15 +40,15 @@ const main = () => {
     
         logseq.provideStyle(String.raw`
     //Bottom
-    body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.flex-1.page.relative {
+    body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.flex-1.page.relative {
         margin-bottom: 2em;
         margin-top: 2em;
         margin-left: 1.5em;
     }
 
     /* right-space */
-    body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
-    body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
+    body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
+    body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
         position: fixed;
         max-height: 210px;
         width: 42vw;
@@ -60,14 +61,14 @@ const main = () => {
     }
 
     /* Pages-tagged-with */
-    body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
+    body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
         bottom: 0;
         right: 0.3em;
     }
 
     /* page-hierarchy */
-    body:not(.is-pdf-active) div#main-content-container div.page-hierarchy,
-    main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
+    body[data-page="page"]:not(.is-pdf-active) div#main-content-container div.page-hierarchy,
+    body[data-page="page"] main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
         right: 42vw;
         bottom: 0;
     }
@@ -82,15 +83,15 @@ const main = () => {
     /* Bottom < 1624 */
     @media screen and (max-width: 1624px) {
 
-        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.flex-1.page.relative {
+        body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.flex-1.page.relative {
             margin-bottom: 2em;
             margin-top: 2em;
             margin-left: 1.5em;
         }
     
         /* right-space */
-        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
-        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
+        body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
+        body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
             position: fixed;
             max-height: 210px;
             width: 42vw;
@@ -103,14 +104,14 @@ const main = () => {
         }
     
         /* Pages-tagged-with */
-        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
+        body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
             bottom: 0;
             right: 0.3em;
         }
     
         /* page-hierarchy */
-        body:not(.is-pdf-active) div#main-content-container div.page-hierarchy,
-        main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
+        body[data-page="page"]:not(.is-pdf-active) div#main-content-container div.page-hierarchy,
+        body[data-page="page"] main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
             right: 42vw;
             bottom: 0;
         }
@@ -120,15 +121,15 @@ const main = () => {
     /* 1625 > Side */
     @media screen and (min-width: 1625px) {
 
-        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.flex-1.page.relative {
+        body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.flex-1.page.relative {
             margin-bottom: 2em;
             margin-top: 2em;
             margin-left: 1.5em;
         }
     
         /* right-space */
-        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
-        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
+        body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
+        body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
             position: fixed;
             width: 390px;
             max-height: 40vh;
@@ -140,44 +141,44 @@ const main = () => {
         }
     
         /* Pages-tagged-with */
-        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
+        body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
             top: 6.5em;
             right: 1em;
         }
     
-        body:not(.is-tabs-loaded.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
+        body[data-page="page"]:not(.is-tabs-loaded.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row {
             top: 4.5em;
         }
     
         /* page-hierarchy */
-        body:not(.is-pdf-active) div#main-content-container div.page-hierarchy,
-        main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
+        body[data-page="page"]:not(.is-pdf-active) div#main-content-container div.page-hierarchy,
+        body[data-page="page"]:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
             right: 1em;
             bottom: 3em;
         }
     } 
     @media screen and (min-width: 1625px) and (max-width: 2259px) {
 
-        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-container {
+        body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-container {
             padding-right: 424px;
         }
 
         /* Contents Page */
-        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
-        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
+        body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
+        body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
             width: 420px;
         }
 
     }
     @media screen and (min-width: 2260px) {
 
-        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-container {
+        body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-container {
             padding-right: 684px;
         }
 
         /* Contents Page */
-        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
-        body:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
+        body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.relative+div.references.mt-6.flex-1.flex-row,
+        body[data-page="page"]:not(.is-pdf-active) main:not(.ls-right-sidebar-open) div#main-content-container div.page-hierarchy {
             width: 680px;
         }
     } 
