943464517460489adcef0e3ed9d6b08dca8c8bfe
YU000jp <111847207+YU000jp@users.noreply.github.com>
2023-07-02 12:53:05 +0900

    fix(bug): Remove console error

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ const main = () => {
                         || current.name.match(/^(\d{4}\/\d{2}\/\d{2})/)
                     )
                 )
-                    (parent.document.querySelector("div.page-hierarchy") as HTMLDivElement)!.classList.add('th-journal');
+                    parent.document.querySelector("div.page-hierarchy")?.classList.add('th-journal');
             })();
         }
     });
