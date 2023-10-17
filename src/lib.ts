f7495c8cc916f50842adab000da45be9df13715e
YU <111847207+YU000jp@users.noreply.github.com>
2023-10-17 17:55:13 +0900

    Cleanup

--- src/lib.ts
+++ src/lib.ts
@@ -18,11 +18,11 @@ export const removeElementId = (elementId: string) => {
   if (doc) doc.remove();
 };
 
-export async function versionCheck(
+export const versionCheck = async (
   first: number,
   second: number,
   third: number
-): Promise<boolean> {
+): Promise<boolean> => {
   let version: string = await logseq.App.getInfo("version");
   //0.9.13-nightly.20230811のような文字列であった場合、-nightly以降を削除する
   if (version?.includes("-")) {
@@ -37,36 +37,36 @@ export async function versionCheck(
   ) {
     return true; //指定した以上のバージョンの場合
   } else return false; //指定より古いバージョンの場合
-}
+};
 
-export function provideStyleByVersion(
+export const provideStyleByVersion = (
   versionOver: boolean,
   newKey: string,
   newStyle: string,
   oldKey: string,
   oldStyle: string
-) {
+) => {
   if (versionOver === true)
     logseq.provideStyle({ key: newKey, style: newStyle });
   //指定した以上のバージョンの場合
   else logseq.provideStyle({ key: oldKey, style: oldStyle }); //指定より古いバージョンの場合
-}
+};
 
-export function titleCollapsedRegisterEvent(
+export const titleCollapsedRegisterEvent = (
   titleElement: HTMLElement,
   targetElement: HTMLElement
-) {
+) => {
   //titleElementをクリックしたらtargetElementのclassを変更する
   if (!titleElement || !targetElement) return;
   titleElement.onclick = async () => {
     if (targetElement.classList.contains("initial")) {
-      //elementTocInPageの親要素のclassをhiddenにする
+      //classをhiddenにする
       targetElement.classList.remove("initial");
       targetElement.classList.add("hidden");
     } else {
-      //elementTocInPageの親要素のclassをinitialにする
+      //classをinitialにする
       targetElement.classList.remove("hidden");
       targetElement.classList.add("initial");
     }
   };
-}
+};
