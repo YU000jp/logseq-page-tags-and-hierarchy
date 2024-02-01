c1bdc2bfa71a758111e4921056e95bf08eadd244
YU <111847207+YU000jp@users.noreply.github.com>
2024-02-01 20:44:34 +0900

    cleanup

--- src/splitHierarchy.ts
+++ src/splitHierarchy.ts
@@ -12,7 +12,8 @@ export const splitHierarchy = async (pageName: string) => {
     if (h1Element === null) return
 
     // 「[[」と「]]」が同時に含まれる場合は、ページ名として認識しない
-    if (pageName.includes("[[") && pageName.includes("]]")) return
+    if (pageName.includes("[[")
+        && pageName.includes("]]")) return
 
     const pageNameArr: string[] = pageName.split('/')
     // pageNameArrの最後の要素
@@ -53,7 +54,7 @@ export const splitHierarchy = async (pageName: string) => {
  */
 export const removeHierarchyPageTitle = async (lastPart: string, fullName: string) => {
     // 日誌は除外
-    const pageTitleElement = parent.document.querySelector("body>div#root>div>main div#main-content-container div.page.relative:not(.is-journals) div.ls-page-title h1.page-title span.title") as HTMLDivElement | null
+    const pageTitleElement = parent.document.body.querySelector("div#root>div>main div#main-content-container div.page.relative:not(.is-journals) div.ls-page-title h1.page-title span.title") as HTMLDivElement | null
     if (!pageTitleElement
         || pageTitleElement.innerText === lastPart) return
     pageTitleElement.innerText = lastPart
@@ -61,21 +62,17 @@ export const removeHierarchyPageTitle = async (lastPart: string, fullName: strin
     setTimeout(() => {
         //同じ階層でspan.editingが発生し消えたらpageTitleElementを元に戻す
         const observer = new MutationObserver((mutations) => {
-            for (const mutation of mutations) {
-                if (mutation.type === "childList") {
-                    for (const node of mutation.removedNodes) {
+            for (const mutation of mutations)
+                if (mutation.type === "childList")
+                    for (const node of mutation.removedNodes)
                         if (node.nodeName === "SPAN") {
                             if (pageTitleElement.innerText === fullName) {
                                 pageTitleElement.innerText = lastPart
                                 return
-                            } else {
+                            } else
                                 //名前が変わっていたら監視を終了
                                 observer.disconnect()
-                            }
                         }
-                    }
-                }
-            };
         })
         const targetNode = pageTitleElement.parentElement
         if (targetNode) observer.observe(targetNode, { childList: true })
@@ -90,13 +87,12 @@ export const removeHierarchyPageTitle = async (lastPart: string, fullName: strin
  * @returns void
  */
 const openPage = async (pageName: string, shiftKey: boolean) => {
-    const page = await logseq.Editor.getPage(pageName) as PageEntity | null//ページの存在チェックが必要
+    const page = await logseq.Editor.getPage(pageName) as { uuid: PageEntity["uuid"] } | null//ページの存在チェックが必要
     if (page) {
-        if (shiftKey) {
+        if (shiftKey)
             logseq.Editor.openInRightSidebar(page.uuid)
-        } else {
+        else
             logseq.App.replaceState('page', { name: pageName })
-        }
     }
 }
 
@@ -105,8 +101,7 @@ const openPage = async (pageName: string, shiftKey: boolean) => {
  * @returns void
  */
 export const revertOnSettingsChangedHierarchyPageTitleOnce = () => {
-    const pageTitleSelector = "div#main-content-container h1.page-title span.title"
-    const pageTitleElement = parent.document.querySelector(pageTitleSelector) as HTMLDivElement | null
+    const pageTitleElement = parent.document.body.querySelector("div#root>div>main div#main-content-container h1.page-title span.title") as HTMLDivElement | null
     if (pageTitleElement) pageTitleElement.innerText = pageTitleElement.dataset.ref as string
 }
 
@@ -115,8 +110,7 @@ export const revertOnSettingsChangedHierarchyPageTitleOnce = () => {
  * @returns void
  */
 export const removeOnSettingsChangedHierarchyPageTitleOnce = () => {
-    const pageTitleSelector = "div#main-content-container h1.page-title span.title"
-    const pageTitleElement = parent.document.querySelector(pageTitleSelector) as HTMLDivElement | null
+    const pageTitleElement = parent.document.body.querySelector("div#root>div>main div#main-content-container h1.page-title span.title") as HTMLDivElement | null
     if (pageTitleElement) {
         const pageTitle = pageTitleElement.innerText
         const pageTitleArr = pageTitle.split("/")
